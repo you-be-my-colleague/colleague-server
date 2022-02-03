451bdc2474c3a39b5a0ff7b06c4beb5d820f5f0c
Jay_ <79740505+Allaccept12@users.noreply.github.com>
2022-02-03 16:17:44 +0900

    [CHORE] 초기 환경 셋팅

---
+++ src/main/java/youBeMyColleague/study/StudyApplication.java
@@ -0,0 +1,13 @@
+package youBeMyColleague.study;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StudyApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(StudyApplication.class, args);
+	}
+
+}
