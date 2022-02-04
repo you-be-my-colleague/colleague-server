4fc074b957a3b1f2be0c5f18ce0432b047b3a965
Jay_ <79740505+Allaccept12@users.noreply.github.com>
2022-02-04 23:21:47 +0900

    [FIX] .yml 파일 로깅 및 프로퍼티 수정, 테스트 코드 작성

--- src/test/java/youBeMyColleague/study/setvice/PostServiceTest.java
+++ src/test/java/youBeMyColleague/study/setvice/PostServiceTest.java
@@ -4,16 +4,20 @@ import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.transaction.annotation.Transactional;
+import youBeMyColleague.study.domain.Member;
+import youBeMyColleague.study.domain.Post;
+import youBeMyColleague.study.domain.TechStack;
+import youBeMyColleague.study.dto.PostRequestDto;
+import youBeMyColleague.study.repository.MemberRepository;
 import youBeMyColleague.study.repository.PostRepository;
 
 import javax.persistence.EntityManager;
-import javax.swing.text.html.parser.Entity;
+import java.util.Optional;
 
-import static org.junit.jupiter.api.Assertions.*;
-
+import static org.assertj.core.api.Assertions.*;
 
 @SpringBootTest
-@Transactional
+@Transactional(readOnly = true)
 class PostServiceTest {
 
     @Autowired
@@ -22,16 +26,24 @@ class PostServiceTest {
     private PostService postService;
     @Autowired
     private PostRepository postRepository;
+    @Autowired
+    private MemberRepository memberRepository;
+
 
 
     @Test
     public void 게시글_등록_테스트() throws Exception {
         //given
-
-
+        Member member = new Member();
+        memberRepository.save(member);
+        System.out.println(member);
         //when
-
+        Post post = postService.createPost(new PostRequestDto("testA", "contentsTest",
+                "ekdmd9092@naver.com", new TechStack("s1", "s2", "s3", "")), member);
         //then
+        Optional<Post> findpost = postRepository.findById(post.getId());
+        assertThat(post).isEqualTo(findpost.get());
+
     }
 
 
