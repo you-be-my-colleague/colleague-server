ab2b5cb05f8be7e1e322fb28313afe44114240e2
Jay_ <79740505+Allaccept12@users.noreply.github.com>
2022-02-04 17:34:45 +0900

    [FIX] 충돌 해결 중

--- src/main/java/youBeMyColleague/study/repository/MemberRepository.java
+++ src/main/java/youBeMyColleague/study/repository/MemberRepository.java
@@ -4,5 +4,4 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import youBeMyColleague.study.domain.Member;
 
 public interface MemberRepository extends JpaRepository<Member,Long> {
-
 }
