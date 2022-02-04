81e1b99b06c1ae44ff55436935eaad4d7f789690
Jay_ <79740505+Allaccept12@users.noreply.github.com>
2022-02-05 01:08:11 +0900

    [FEAT] PostService 로직 수정

--- src/main/java/youBeMyColleague/study/domain/Member.java
+++ src/main/java/youBeMyColleague/study/domain/Member.java
@@ -5,6 +5,7 @@ import com.fasterxml.jackson.annotation.JsonIgnore;
 import lombok.*;
 
 import javax.persistence.*;
+import java.util.ArrayList;
 import java.util.List;
 
 @Entity
--- src/test/java/youBeMyColleague/study/service/PostServiceTest.java
+++ src/test/java/youBeMyColleague/study/service/PostServiceTest.java
@@ -1,5 +1,6 @@
-package youBeMyColleague.study.setvice;
+package youBeMyColleague.study.service;
 
+import lombok.extern.slf4j.Slf4j;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
@@ -17,7 +18,8 @@ import java.util.Optional;
 import static org.assertj.core.api.Assertions.*;
 
 @SpringBootTest
-@Transactional(readOnly = true)
+@Transactional
+@Slf4j
 class PostServiceTest {
 
     @Autowired
