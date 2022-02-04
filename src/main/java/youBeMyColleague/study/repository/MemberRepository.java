cc832765d0394e88f13f3b9d52c17ed6c96f0799
Jay_ <79740505+Allaccept12@users.noreply.github.com>
2022-02-04 16:53:20 +0900

    [FIX] 충돌 문제 해결중

--- src/main/java/youBeMyColleague/study/repository/MemberRepository.java
+++
@@ -1,8 +0,0 @@
-package youBeMyColleague.study.repository;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-import youBeMyColleague.study.domain.Member;
-
-public interface MemberRepository extends JpaRepository<Member, Long> {
-
-}
