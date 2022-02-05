0cbddcd5ff7f41db07b4d7a8bcb4515ca6db551d
alisyabob <sjs939883@gmail.com>
2022-02-06 08:04:49 +0900

    [FEAT] 네이버 로그인 구현 완료

--- src/main/java/youBeMyColleague/study/config/oauth/PrincipalOauth2UserService.java
+++ src/main/java/youBeMyColleague/study/config/oauth/PrincipalOauth2UserService.java
@@ -7,9 +7,12 @@ import org.springframework.security.oauth2.core.OAuth2AuthenticationException;
 import org.springframework.security.oauth2.core.user.OAuth2User;
 import org.springframework.stereotype.Service;
 import youBeMyColleague.study.config.oauth.provider.GoogleUserInfo;
+import youBeMyColleague.study.config.oauth.provider.NaverUserInfo;
 import youBeMyColleague.study.config.oauth.provider.OAuth2UserInfo;
 import youBeMyColleague.study.domain.Member;
 import youBeMyColleague.study.repository.MemberRepository;
+
+import java.util.Map;
 
 @Service
 @RequiredArgsConstructor
@@ -29,6 +32,9 @@ public class PrincipalOauth2UserService extends DefaultOAuth2UserService {
         if(userRequest.getClientRegistration().getRegistrationId().equals("google")){
             System.out.println("구글");
             oAuth2UserInfo = new GoogleUserInfo(oAuth2User.getAttributes());
+        }else if(userRequest.getClientRegistration().getRegistrationId().equals("naver")){
+            System.out.println("네이버");
+            oAuth2UserInfo = new NaverUserInfo((Map)oAuth2User.getAttributes().get("response"));
         }
 
         assert oAuth2UserInfo != null;
