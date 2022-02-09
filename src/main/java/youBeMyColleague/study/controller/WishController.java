7dcaaf5281584540abea7d91e0beb48f4af36a92
alisyabob <sjs939883@gmail.com>
2022-02-10 02:34:08 +0900

    [FIX] wishList 서비스 생성

--- src/main/java/youBeMyColleague/study/controller/WishController.java
+++ src/main/java/youBeMyColleague/study/controller/WishController.java
@@ -11,6 +11,7 @@ import youBeMyColleague.study.dto.WishResponseDto;
 import youBeMyColleague.study.model.GetAllWishList;
 import youBeMyColleague.study.model.Success;
 import youBeMyColleague.study.service.MemberService;
+import youBeMyColleague.study.service.WishService;
 
 import java.util.List;
 import java.util.Optional;
@@ -21,11 +22,11 @@ import java.util.stream.Collectors;
 @RequiredArgsConstructor
 public class WishController {
 
-    private final MemberService memberService;
+    private final WishService wishService;
     //마이페이지 내 관심글
     @GetMapping("/post-like/{member_id}/{post_id}")
     public ResponseEntity<GetAllWishList> selectLikePost(@PathVariable Long member_id, @PathVariable Long post_id){
-        List<WishList> memberLikePost = Optional.of(memberService.findLikePost(member_id, post_id))
+        List<WishList> memberLikePost = Optional.of(wishService.findLikePost(member_id, post_id))
                 .orElseThrow(WishListNotFoundException::new);
         return new ResponseEntity<>(new GetAllWishList(true,"내 관심글 조회성공",memberLikePost.stream()
                 .map(WishResponseDto::new)
@@ -35,14 +36,14 @@ public class WishController {
     //관심글 등록
     @PostMapping("/post-like/{member_id}/{post_id}")
     public ResponseEntity<Success> createLikePost (@PathVariable Long member_id, @PathVariable Long post_id){
-        Optional.of(memberService.createLikePost(member_id, post_id)).orElseThrow(WishListCreateException::new);
+        Optional.of(wishService.createLikePost(member_id, post_id)).orElseThrow(WishListCreateException::new);
         return new ResponseEntity<>(new Success(true,"내 관심글 등록 성공"),HttpStatus.OK);
     }
 
     // 관심글 삭제
     @DeleteMapping("/my-page/my-likes/{member_id}/{post_id}")
     public ResponseEntity<Success> deleteLikePost(@PathVariable Long member_id, @PathVariable Long post_id){
-        memberService.deleteLikePost(member_id,post_id);
+        wishService.deleteLikePost(member_id,post_id);
         return new ResponseEntity<>(new Success(true, "내 관심글 삭제 성공"),HttpStatus.OK);
     }
 }
